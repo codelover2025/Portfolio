c3954f011f8522644d66701d87fabe76eafbb6a2
Chirag <chirag19saxena2001@gmail.com>
2025-06-04 15:30:02 +0530

    initial commit

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: "/https://github.com/codelover2025/Portfolio/", // replace with your GitHub repo name
+base: "/chirag-portfolio/",
   plugins: [react()],
 })
