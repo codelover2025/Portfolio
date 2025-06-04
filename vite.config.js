f50c7f490b314e99674675c46f3d7acd830e956a
Chirag <chirag19saxena2001@gmail.com>
2025-06-04 15:42:36 +0530

    initial commit

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const basePath = process.env.BASE_PATH || "/";
+
 export default defineConfig({
-base: "/chirag-portfolio/",
+  base: basePath,
   plugins: [react()],
 })
